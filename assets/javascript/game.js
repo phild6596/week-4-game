c5c1471734053257b53e5f1529f84fe6b5e3eda5
phild6596 <phild6596@gmail.com>
2017-12-12 14:01:30 -0800

    <Updated JQuery> Updated JQuery to include a random number generator

---
+++ assets/javascript/game.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+var min = 19;
+var max = 120;
+var randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+var wins = 0;
+var losses = 0;
+var totalScore = "";
+var numberString1 = "";
+var numberString2 = "";
+
+console.log("Jaden=" + randomNumber);
+
+
+
+
+
+});
