849f0f04e1a64182acd892b8a3b83891f0885a83
phild6596 <phild6596@gmail.com>
2017-12-12 18:22:25 -0800

    <Updated JS> Updated JS to include click Jquery on jewels. Numbers now add and give total score.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,16 +2,40 @@ $(document).ready(function(){
 var min = 19;
 var max = 120;
 var randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+var blueCrystal = Math.floor(Math.random() * 12) +1;
+var greenCrystal = Math.floor(Math.random() * 12) + 1;
+var navyCrystal = Math.floor(Math.random() * 12) + 1;
+var redCrystal = Math.floor(Math.random() * 12) + 1;
 var wins = 0;
 var losses = 0;
-var totalScore = "";
+var totalScore = 0;
 var numberString1 = "";
 var numberString2 = "";
 
-console.log("Jaden=" + randomNumber);
+console.log("rando number " + randomNumber);
 
+//random number to HTML
+$(".random-number").html(randomNumber);
 
+$("#blue-crystal").on("click", function() {
+    totalScore += blueCrystal;
+    $(".total-score").html(totalScore);
+})
 
+$("#green-crystal").on("click", function () {
+    totalScore += greenCrystal;
+    $(".total-score").html(totalScore);
+})
+
+$("#navy-crystal").on("click", function () {
+    totalScore += navyCrystal;
+    $(".total-score").html(totalScore);
+})
+
+$("#red-crystal").on("click", function () {
+    totalScore += redCrystal;
+    $(".total-score").html(totalScore);
+})
 
 
 });
